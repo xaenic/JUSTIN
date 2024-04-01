65494ae6ab695d8f9f902c27bc037b9f104a8623
Kiwi <allanvillegas35@gmail.com>
2024-04-02 02:36:26 +0800

    sorry tin ga dali najud ko duka na kaayo ko hehehe <3

--- register_form.php
+++ register_form.php
@@ -38,6 +38,7 @@ if(isset($_POST['submit'])){
 <!DOCTYPE html>
 <html lang="en">
 <head>
+   <!-- sorrtitn -->
    <meta charset="UTF-8">
    <meta http-equiv="X-UA-Compatible" content="IE=edge">
    <meta name="viewport" content="width=device-width, initial-scale=1.0">
